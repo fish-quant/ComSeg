4533b66de9ddd328e03248194a8f40c020e8c8c3
tdefa <thomas.defard@minesparistech.fr>
2023-11-18 20:36:52 +0100

    import

--- src/comseg/dictionary.py
+++ src/comseg/dictionary.py
@@ -19,8 +19,6 @@ then apply a community classification (in situ clustering class)
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
 from tqdm import tqdm
 
 import anndata as ad
