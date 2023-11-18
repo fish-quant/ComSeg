c6252584bbe070b471769f6e66c3716f800cc2f1
tdefa <thomas.defard@minesparistech.fr>
2023-11-18 20:27:12 +0100

    import

--- src/comseg/__init__.py
+++ src/comseg/__init__.py
@@ -1,14 +1,15 @@
 
 
+from .clustering import InSituClustering
 
-from . import model
-from . import clustering
-from . import model
+#from . import model
+#from . import clustering
+#from . import model
 
 
-from . import utils
-from .utils.preprocessing import run_sctransform, select_genes_for_sct
-from .utils.preprocessing import sctransform_from_parameters
+#from . import utils
+#from .utils.preprocessing import run_sctransform, select_genes_for_sct
+#from .utils.preprocessing import sctransform_from_parameters
 #from .utils import preprocessing
 #from .utils import *
 
